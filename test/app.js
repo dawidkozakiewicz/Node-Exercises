a26c12bf614cb56792295fa3f5907b98e1bc1d66
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-04-03 22:37:17 +0200

    add yargs

--- test/app.js
+++ test/app.js
@@ -1,4 +1,21 @@
-const chalk = require('chalk');
- 
-console.log(chalk.bold.yellow.underline('Success!'));
-console.log(chalk.green.underline('Success!'));
+const yargs = require('yargs')
+
+
+yargs.command({
+    command: 'add',
+    describe: 'Add notes',
+    handler: () => {
+        console.log('Adding note...')
+    }
+})
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove notes',
+    handler: () => {
+        console.log('Removing note...')
+    }
+})
+
+console.log(process.argv)
+console.log(yargs.argv)
