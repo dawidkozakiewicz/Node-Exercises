e59caf4380c9e83110f4679d352e277657d1d2c1
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-04-18 20:23:05 +0200

    adding new functions

--- 3_node_js_module_system/n3-04-07-removing-note/notes-app/notes.js
+++ 3_node_js_module_system/n3-04-07-removing-note/notes-app/notes.js
@@ -5,14 +5,11 @@ const getNotes = function () {
     return 'Your notes...'
 }
 
-
-const addNote = (title, body) => {
+const addNote = function (title, body) {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(
-        (note) => {
-            return note.title === title
-        }
-    )
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -24,33 +21,28 @@ const addNote = (title, body) => {
     } else {
         console.log(chalk.red.inverse('Note title taken!'))
     }
-
-    
 }
 
-const removeNote = (title) => {
+const removeNote = function (title) {
     const notes = loadNotes()
-    const notesToKeep = notes.filter(
-        (note) => {
-            return note.title !== title
-        }
-    )
+    const notesToKeep = notes.filter(function (note) {
+        return note.title !== title
+    })
+
     if (notes.length > notesToKeep.length) {
         console.log(chalk.green.inverse('Note removed!'))
         saveNotes(notesToKeep)
     } else {
         console.log(chalk.red.inverse('No note found!'))
-    }
-    
+    }    
 }
 
-const saveNotes = (notes) => {
+const saveNotes = function (notes) {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-
-const loadNotes = () => {
+const loadNotes = function () {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
