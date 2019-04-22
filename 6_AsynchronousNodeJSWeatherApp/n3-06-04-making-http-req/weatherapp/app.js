6d00d93501f87681b386dcfd961e3874255b441b
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-04-22 23:08:40 +0200

    add http-req

--- 6_AsynchronousNodeJSWeatherApp/n3-06-04-making-http-req/weatherapp/app.js
+++ 6_AsynchronousNodeJSWeatherApp/n3-06-04-making-http-req/weatherapp/app.js
@@ -1,8 +1,11 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/adc8e1765abbd4506c2e98e7d14f5495/51.1000000,17.0333300'
+const url = 'https://api.darksky.net/forecast/adc8e1765abbd4506c2e98e7d14f5495/51.1000000,17.0333300?units=si&lang=pl'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+    const data = response.body
+    const temperature = data.currently.temperature
+    const precipProbability = data.currently.precipProbability
+    const allday = data.daily.data[0].summary
+    console.log(`Jet aktualnie ${temperature} stopni Celsjusza. Prawdopodobieństwo deszczu wynosi ${precipProbability}% Przewidywana pogoda: ${allday}`)
 })
