b562e91d8f933f85a2dbfc54f89aa24843b1b2f5
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-04-24 20:33:41 +0200

    geocode challenge

--- 6_AsynchronousNodeJSWeatherApp/n3-06-06-geocode/weather-app/app.js
+++ 6_AsynchronousNodeJSWeatherApp/n3-06-06-geocode/weather-app/app.js
@@ -1,18 +1,28 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-122.4233'
+// const url = 'https://api.darksky.net/forecast/adc8e1765abbd4506c2e98e7d14f5495/51.10773,17.03533?units=si&lang=pl'
 
-request({ url: url, json: true }, (error, response) => {
-    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
-})
+// request({ url: url, json: true }, (error, response) => {
+//     console.log(response.body.daily.data[0].summary + ' Aktualnie mamy ' + response.body.currently.temperature + ' stopni. Prawdopodobieństwo deszczu wynosi: ' + response.body.currently.precipProbability + ' %')
+// })
 
 // Geocoding
 // Address -> Lat/Long -> Weather
 
-const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Lisboa.json?access_token=pk.eyJ1IjoidHVyZ29yMTk5NiIsImEiOiJjanV2aDYyazMwMWt5M3pxaHl0MmNtNzNhIn0.Fe0DCxTICfJ9zzbbxv9bmw'
 
 request({ url: geocodeURL, json: true }, (error, response) => {
     const latitude = response.body.features[0].center[0]
     const longitude = response.body.features[0].center[1]
     console.log(latitude, longitude)
 })
+
+
+const restCountries ='https://restcountries.eu/rest/v2/all'
+
+request({url: restCountries, json: true}, (error, response) => {
+    const country = response.body[0].nativeName
+    console.log(`Debile sami na siebie mówią tak: ${country}`)
+      
+    })
+    
