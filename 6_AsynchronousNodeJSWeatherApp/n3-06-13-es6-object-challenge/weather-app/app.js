ae2964f09494baa670bb693aae39304ab86f82b8
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-05-10 23:12:25 +0200

    good old-fashioned HTTP request

--- 6_AsynchronousNodeJSWeatherApp/n3-06-13-es6-object-challenge/weather-app/app.js
+++ 6_AsynchronousNodeJSWeatherApp/n3-06-13-es6-object-challenge/weather-app/app.js
@@ -1,6 +1,5 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
-
 const address = process.argv[2]
 
 if (!address) {
@@ -21,3 +20,36 @@ if (!address) {
         })
     })
 }
+
+// const geocode = (address, callback) => {
+//     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+
+//     request({ url, json: true }, (error, { body }) => {
+//         if (error) {
+//             callback('Unable to connect to location services!', undefined)
+//         } else if (body.features.length === 0) {
+//             callback('Unable to find location. Try another search.', undefined)
+//         } else {
+//             callback(undefined, {
+//                 latitude: body.features[0].center[1],
+//                 longitude: body.features[0].center[0],
+//                 location: body.features[0].place_name
+//             })
+//         }
+//     })
+// }
+
+// const forecast = (latitude, longitude, callback) => {
+//     const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/' + latitude + ',' + longitude
+
+//     request({ url, json: true }, (error, { body }) => {
+//         if (error) {
+//             callback('Unable to connect to weather service!', undefined)
+//         } else if (body.error) {
+//             callback('Unable to find location', undefined)
+//         } else {
+//             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+//         }
+//     })
+// }
+
