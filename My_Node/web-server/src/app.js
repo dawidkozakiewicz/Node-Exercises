049af044873ccf4d08e9dd2f66f95b8659b8a773
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-05-24 19:47:54 +0200

    deleting unnecessary folders

--- My_Node/web-server/src/app.js
+++ My_Node/web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -20,14 +22,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Abraham Breslauer'
+        name: 'Andrew Mead'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Abraham Breslauer'
+        name: 'Andrew Mead'
     })
 })
 
@@ -35,7 +37,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Abraham Breslauer'
+        name: 'Andrew Mead'
     })
 })
 
@@ -46,10 +48,22 @@ app.get('/weather', (req, res) => {
         })
     }
 
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia',
-        address: req.query.address
+    geocode(req.query.address, (error, { latitude, longitude, location }) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
 })
 
@@ -62,14 +76,14 @@ app.get('/products', (req, res) => {
 
     console.log(req.query.search)
     res.send({
-        products: [req.query.search]
+        products: []
     })
 })
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Abraham Breslauer',
+        name: 'Andrew Mead',
         errorMessage: 'Help article not found.'
     })
 })
@@ -77,11 +91,11 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Abraham Breslauer',
+        name: 'Andrew Mead',
         errorMessage: 'Page not found.'
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(3002, () => {
+    console.log('Server is up on port 2000.')
 })
