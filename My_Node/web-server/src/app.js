366ddffeb2750174c7181df1ca03340f8fd15987
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-05-28 22:41:09 +0200

    creating form

--- My_Node/web-server/src/app.js
+++ My_Node/web-server/src/app.js
@@ -83,7 +83,7 @@ app.get('/products', (req, res) => {
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Abraham Breslauer',
+        name: 'AAbraham Breslauer',
         errorMessage: 'Help article not found.'
     })
 })
@@ -91,7 +91,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Abraham Breslauer',
+        name: 'AAbraham Breslauer',
         errorMessage: 'Page not found.'
     })
 })
