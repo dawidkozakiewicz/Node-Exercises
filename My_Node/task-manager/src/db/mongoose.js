a40f39786a8cf0c7e9540c649cbcb7e5b7255d12
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-06-12 19:56:48 +0200

    add new features in validator

--- My_Node/task-manager/src/db/mongoose.js
+++ My_Node/task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
@@ -7,20 +8,73 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
     }
 })
 
-const me = new User({
-    name: 'Dawid',
-    age: '40'
+// const me = new User({
+//     name: '   Andrew  ',
+//     email: 'MYEMAIL@MEAD.IO   ',
+//     password: 'phone098!'
+// })
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
-me.save().then(() => {
-    console.log(me)
+const task = new Task({
+    description: '  Eat lunch'
+})
+
+task.save().then(() => {
+    console.log(task)
 }).catch((error) => {
-    console.log('Error!', error)
+    console.log(error)
 })
